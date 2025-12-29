65ecd6b57e0a663c1c08a6fd916335e2909ad18c
Mehmet Çağrı Karataş <mehmetcagrikaratas@gmail.com>
2025-12-29 15:07:42 -0500

    .

--- app/Filament/Resources/ReconciliationRequestResource/Pages/ListReconciliationRequests.php
+++ app/Filament/Resources/ReconciliationRequestResource/Pages/ListReconciliationRequests.php
@@ -13,7 +13,9 @@ class ListReconciliationRequests extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Yeni Mutabakat Talebi'),
+        
         ];
     }
 }
